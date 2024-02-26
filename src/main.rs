002be4ac267b5ddcb468dd1ed070b5f28ed24ca2
Amit Jakubowicz <amiiit@users.noreply.github.com>
2024-02-26 19:04:07 +0100

    Compiling

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,23 @@ use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
 use esp_idf_svc::wifi::EspWifi;
-use log::info;
 
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
-    info!("!!!IMAGE TWO!!!");
 
     let peripherals = Peripherals::take().unwrap();
     let sysloop = EspSystemEventLoop::take()?;
     let nvs = EspDefaultNvsPartition::take()?;
 
-    let mut wifi = EspWifi::new(peripherals.modem, sysloop, Some(nvs))?;
-    wifi.set_configuration(&Configuration::Client(ClientConfiguration {
-        ssid: "brisa".into(),
-        password: "kalicanelo".into(),
-        auth_method: AuthMethod::None,
-        ..Default::default()
-    }))?;
+let mut wifi = EspWifi::new(peripherals.modem, sysloop, Some(nvs))?;
+
+wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+    ssid: "brisa".try_into().unwrap(),
+    password: "kalicanelo".try_into().unwrap(),
+    auth_method: AuthMethod::None,
+    ..Default::default()
+}))?;
 
     wifi.start()?;
     wifi.connect()?;
@@ -35,7 +34,6 @@ fn main() -> anyhow::Result<()> {
     }
 
     println!("Connected");
-    my_ota::my_ota::ota_update_handler(env!("VERSION"), env!("VERSION"))?;
+    my_ota::my_ota::ota_update_handler(env!("CARGO_PKG_VERSION"), "device_id")?;
     Ok(())
-
 }
--- src/my_ota.rs
+++ src/my_ota.rs
@@ -3,113 +3,89 @@ pub mod my_ota {
     use esp_idf_svc::ota::EspOta;
     use log::info;
     use embedded_svc::http::Method;
-    use esp_idf_sys::EspError;
 
-    fn fetch_target_firmware_url(device_id: &str) -> anyhow::Result<String, EspError> {
+    const BUF_MAX: usize = 2048;
+    const TARGET_VERSION_MAX_LENGTH: usize = 64;
+
+    fn fetch_target_firmware_version(device_id: &str) -> anyhow::Result<String, anyhow::Error> {
+        let mut client = EspHttpConnection::new(&Configuration {
+            crt_bundle_attach: Some(esp_idf_sys::esp_crt_bundle_attach),
+            buffer_size: Some(BUF_MAX),
+            ..Default::default()
+        })?;
+
+        let device_specific_url = format!("https://storage.googleapis.com/devices/{}/target", device_id);
+        info!("Requesting target version via device-specific URL: {}", device_specific_url);
+
+        let _resp = client.initiate_request(Method::Get, &device_specific_url, &[]);
+        client.initiate_response()?;
+
+
+        // let content_length = client.header("Content-Length")?;
+
+
+        if client.status() != 200 {
+            info!("Error fetching target for device from {}", device_specific_url);
+            return Err(anyhow::Error::msg("Failed to fetch target version"));
+        }
+
+        let mut target_version_body = [0; TARGET_VERSION_MAX_LENGTH];
+        let target_version_bytesread = client.read(&mut target_version_body)?;
+
+        let result = String::from_utf8(target_version_body[..target_version_bytesread].to_vec())
+            .map_err(|err| anyhow::Error::new(err))?;
+
+        Ok(result)
+    }
+
+    pub fn do_update_if_available(current_version: &str, device_id: &str) -> anyhow::Result<()> {
+        match fetch_target_firmware_version(device_id) {
+            Ok(target_version) => Ok(()),
+            Err(target_version) => Err(anyhow::Error::msg("couldnt get version"))
+        }
+
+    }
+
+    pub fn ota_update_handler(current_version: &str, device_id: &str) -> anyhow::Result<()> {
+        info!("Start processing OTA update");
+
         const BUF_MAX: usize = 2 * 1024;
+        let mut body = [0; BUF_MAX];
+
+        let firmware_url = "https://storage.googleapis.com/devices/image.bin";
+        info!("Using firmware from: {}", firmware_url);
+
+        let mut ota = EspOta::new()?;
+        let mut ota_update = ota.initiate_update()?;
 
         let mut client = EspHttpConnection::new(&Configuration {
             crt_bundle_attach: Some(esp_idf_sys::esp_crt_bundle_attach),
             buffer_size: Some(BUF_MAX),
             ..Default::default()
-        })
-            .expect("creation of EspHttpConnection should have worked");
+        })?;
 
-        let device_specific_url = format!("https://storage.googleapis.com/devices/{}/target", device_id);
-        info!("Will request target version via device specific URL: {}", device_specific_url);
-        let _resp = client.initiate_request(Method::Get, &device_specific_url, &[]);
-        const TARGET_VERSION_MAX_LENGTH: usize = 64;
-        let mut target_version_body: [u8; TARGET_VERSION_MAX_LENGTH] = [0; TARGET_VERSION_MAX_LENGTH];
-        client.initiate_response()?;
-        let mut content_length: usize = 0;
-
-        if let Some(len) = client.header("Content-Length") {
-            content_length = len.parse().unwrap();
-        } else {
-            info!("reading content length for target version http request failed");
-        }
-        if client.status() != 200 {
-            info!("Error fetching target for device from {}", device_specific_url);
-            return anyhow::Error();
-        }
-
-        let target_version_bytesread = match client.read(&mut target_version_body)  {
-            Ok(n) => n,
-            Err(err) => {
-                info!("Error reading body for target URL {:?}", err);
-                Err(anyhow::Error());
-            }
-        }
-
-        let result = match std::string::String::from_utf8(target_version_body.to_vec()) {
-            Ok(s) => s,
-            Err(err) => {
-                info!("Error parding response into string {:?}", err);
-                Err(anyhow::Error())
-            }
-        };
-
-        Ok(result)
-    }
-    pub fn ota_update_handler(current_version: &str, device_id: &str) -> anyhow::Result<()> {
-
-
-        let mut firmware_update_ok = false;
-
-        info!("Start processing OTA update");
-
-        let mut content_length: usize = 0;
-        const BUF_MAX: usize = 2 * 1024;
-        let mut body: [u8; BUF_MAX] = [0; BUF_MAX];
-
-        let firmware_url = "https://storage.googleapis.com/devices/image.bin";
-        info!("Will use firmware from: {}", firmware_url);
-
-        let mut ota = EspOta::new().expect("EspOta::new should have been successful");
-
-        let mut ota_update = ota
-            .initiate_update()
-            .expect("initiate ota update should have worked");
-
-        info!("EspHttpConnection created");
-        let _resp = client.initiate_request(embedded_svc::http::Method::Get, firmware_url, &[]);
-
-        info!("after client.initiate_request()");
-
+        let _resp = client.initiate_request(Method::Get, firmware_url, &[]);
         client.initiate_response()?;
 
-        if let Some(len) = client.header("Content-Length") {
-            content_length = len.parse().unwrap();
-        } else {
-            info!("reading content length for firmware update http request failed");
-        }
+        let content_length = client.header("Content-Length")
+            .map(|len| len.parse().unwrap_or_default())
+            .unwrap_or_default();
 
         info!("Content-length: {:?}", content_length);
-        info!(">>>>>>>>>>>>>>>> initiating OTA update");
+        info!("Initiating OTA update");
 
         let mut bytes_read_total = 0;
 
         loop {
             esp_idf_hal::delay::FreeRtos::delay_ms(10);
-            let n_bytes_read = match client.read(&mut body) {
-                Ok(n) => n,
-                Err(err) => {
-                    info!("ERROR reading firmware batch {:?}", err);
-                    break;
-                }
-            };
+            let n_bytes_read = client.read(&mut body)?;
+
             bytes_read_total += n_bytes_read;
 
-            if !body.is_empty() {
-                match ota_update.write(&body) {
-                    Ok(_) => {}
-                    Err(err) => {
-                        info!("ERROR failed to write update with: {:?}", err);
-                        break;
-                    }
-                }
+            if !body[..n_bytes_read].is_empty() {
+                ota_update.write(&body[..n_bytes_read])?;
             } else {
-                info!("ERROR firmware image with zero length");
+                info!("ERROR: Firmware image with zero length");
                 break;
             }
 
@@ -119,24 +95,20 @@ pub mod my_ota {
         }
 
         if bytes_read_total == content_length {
-            firmware_update_ok = true;
+            ota_update.complete()?;
+            info!("Successfully completed firmware update");
+        } else {
+            ota_update.abort()?;
+            info!("ERROR: Firmware update failed");
+            return Err(anyhow::Error::msg("Firmware update failed"));
         }
 
-        let _confirmation_msg = if firmware_update_ok {
-            ota_update.complete().unwrap();
-            info!("completed firmware update");
-            info!("Successfully completed firmware update")
-        } else {
-            ota_update.abort().unwrap();
-            info!("ERROR firmware update failed");
-            info!("Firmare update failed")
-        };
-
         esp_idf_hal::delay::FreeRtos::delay_ms(1000);
-        info!("restarting device after firmware update");
+        info!("Restarting device after firmware update");
         unsafe {
             esp_idf_sys::esp_restart();
         }
+
+        Ok(())
     }
 }
-
